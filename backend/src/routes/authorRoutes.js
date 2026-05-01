1b35b9de7c1629a068c73399b82bc0d53deb64ac
shehan1031 <10952642@students.plymouth.ac.uk>
2026-05-01 15:23:15 +0530

    Update authorRoutes.js

--- backend/src/routes/authorRoutes.js
+++ backend/src/routes/authorRoutes.js
@@ -1,4 +1,3 @@
-const express = require("express");
 const router  = require("express").Router();
 const auth    = require("../middleware/authMiddleware");
 const { getAuthorNovels }           = require("../controllers/novelController");
